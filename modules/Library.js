2b0877b1617f4c239ab8b87017496ab41db925e7
Juanpa8830 <jp3_4@hotmail.com>
2022-12-06 11:09:43 -0500

    fix linter errors

--- modules/Library.js
+++ modules/Library.js
@@ -1,16 +1,15 @@
 export default class Library {
-  constructor(){
+  constructor() {
     this.library = [];
   }
-  
+
   addBook(book) {
     this.library.push(book);
     localStorage.setItem('Books', JSON.stringify(this.library));
   }
-  
+
   loadContent() {
-    
-    const content = localStorage.getItem("book")? JSON.parse(localStorage.getItem('Books')): null;
+    const content = localStorage.getItem('book') ? JSON.parse(localStorage.getItem('Books')) : null;
     if (content === 0 || content === null) {
       localStorage.setItem('Books', JSON.stringify(this.library));
     } else {
